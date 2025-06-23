3d75fa9c5106c216c41ac47a152be268e59fbaae
Matheus M Cavazotti <mcavazotti@gmail.com>
2025-06-23 15:59:45 -0300

    Theme + logo

--- website/src/components/theme.tsx
+++ website/src/components/theme.tsx
@@ -1,5 +1,47 @@
 import { createTheme, extendTheme } from "@mui/material";
 
 export const TABTheme = createTheme({
-  cssVariables: true,
+  colorSchemes: {
+    light: {
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#5d4037",
+        },
+        secondary: {
+          main: "#00897b",
+        },
+        background: {
+          default: "#fff8e1",
+          paper: "#fff5cc",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#ffcc80",
+        },
+        secondary: {
+          main: "#4db6ac",
+        },
+        background: {
+          default: "#221b1b",
+          paper: "#1c1a17",
+        },
+      },
+    },
+  },
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+
+  typography: {
+    fontFamily: "Montserrat",
+  },
+  spacing: 4,
+  shape: {
+    borderRadius: 0,
+  },
 });
--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -1,9 +1,11 @@
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
 import { TABTheme } from "../components/theme";
+import React from "react";
 
 export default function Root({ children }: { children: ReactNode[] }) {
   return <ThemeProvider theme={TABTheme}>
+    <CssBaseline/>
     {children}
     </ThemeProvider>;
 }
