d021556acadea7e0ddb15feb5791f0f27f2afd05
Matheus Cavazotti <mcavazotti@gmail.com>
2025-06-25 22:10:54 -0300

    Math support

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -3,9 +3,15 @@ import starlight from "@astrojs/starlight";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import githubData from "./src/config/github-data";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
+  },
   integrations: [
     react(),
     starlight({
@@ -14,10 +20,18 @@ export default defineConfig({
         src: "./src/assets/Logo.png",
       },
       customCss: ["./src/styles/custom.css"],
-
       social: [{ icon: "github", label: "GitHub", href: githubData.repositoryAddress() }],
       favicon: "favicon.ico",
       credits: true,
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            href: "https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css",
+            rel: "stylesheet",
+          },
+        },
+      ],
       sidebar: [
         {
           label: "Code",
