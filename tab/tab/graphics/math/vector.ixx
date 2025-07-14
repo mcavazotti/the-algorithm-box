108c0ba503b4e2a3a3f4b08f5f32a6981ebabc53
Matheus M Cavazotti <mcavazotti@gmail.com>
2025-07-14 14:12:28 -0300

    Add Vector4

--- tab/tab/graphics/math/vector.ixx
+++ tab/tab/graphics/math/vector.ixx
@@ -18,7 +18,7 @@ public:
     Vector2 operator-(const Vector2& other) const { return {x - other.x, y - other.y}; }
     Vector2 operator*(const Vector2& other) const { return {x * other.x, y * other.y}; }
     Vector2 operator/(const Vector2& other) const { return {x / other.x, y / other.y}; }
-    Vector2 operator-() const { return {-x, -y}; };
+    Vector2 operator-() const { return {-x, -y}; }
     // end-basic-ops
 
     // start-scalar-ops
@@ -52,7 +52,7 @@ public:
     Vector3 operator-(const Vector3& other) const { return { x - other.x, y - other.y, z - other.z }; }
     Vector3 operator*(const Vector3& other) const { return { x * other.x, y * other.y, z * other.z }; }
     Vector3 operator/(const Vector3& other) const { return { x / other.x, y / other.y, z / other.z }; }
-    Vector3 operator-() const { return { -x, -y, -z }; };
+    Vector3 operator-() const { return { -x, -y, -z }; }
     Vector3 operator*(const double other) const { return { x * other, y * other, z * other }; }
     Vector3 operator/(const double other) const { return { x / other, y / other, z / other }; }
     bool operator==(const Vector3& other) const { return x == other.x && y == other.y && z == other.z; }
@@ -66,6 +66,27 @@ public:
     [[nodiscard]] Vector3 cross(const Vector3& other) const { return {y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x }; }
     // start-cross
 
-
     double x, y, z;
+};
+
+export class Vector4 {
+public:
+    Vector4() : x(0), y(0), z(0), w(0) {}
+    Vector4(const double x, const double y, const double z, const double w) : x(x), y(y), z(z), w(w) {}
+
+    Vector4 operator+(const Vector4& other) const { return { x + other.x, y + other.y, z + other.z, w + other.w }; }
+    Vector4 operator-(const Vector4& other) const { return { x - other.x, y - other.y, z - other.z, w - other.w }; }
+    Vector4 operator*(const Vector4& other) const { return { x * other.x, y * other.y, z * other.z, w * other.w }; }
+    Vector4 operator/(const Vector4& other) const { return { x / other.x, y / other.y, z / other.z, w / other.w }; }
+    Vector4 operator-() const { return { -x, -y, -z, -w }; }
+    Vector4 operator*(const double other) const { return { x * other, y * other, z * other, w * other }; }
+    Vector4 operator/(const double other) const { return { x / other, y / other, z / other, w / other }; }
+    bool operator==(const Vector4& other) const { return x == other.x && y == other.y && z == other.z && w == other.w; }
+    bool operator!=(const Vector4& other) const { return !(*this == other); }
+
+    [[nodiscard]] double length() const { return sqrt(x * x + y * y + z * z + w * w); };
+    [[nodiscard]] Vector4 normalize() const { return *this / length(); };
+    [[nodiscard]] double dot(const Vector4& other) const { return x * other.x + y * other.y + z * other.z + w * other.w; }
+
+    double x, y, z, w;
 };
--- tab/tests/graphics/math/vector_test.cpp
+++ tab/tests/graphics/math/vector_test.cpp
@@ -258,3 +258,146 @@ TEST(VectorTest, v3Cross) {
     EXPECT_EQ(v1.cross(v2), -v3);
     EXPECT_EQ(v4.cross(v3), v5);
 }
+
+TEST(VectorTest, v4DefaultConstructor) {
+    const Vector4 v;
+    EXPECT_EQ(v.x, 0);
+    EXPECT_EQ(v.y, 0);
+    EXPECT_EQ(v.z, 0);
+    EXPECT_EQ(v.w, 0);
+}
+
+TEST(VectorTest, v4Initialization) {
+    const Vector4 v(1, 2, 3, 4);
+    EXPECT_EQ(v.x, 1);
+    EXPECT_EQ(v.y, 2);
+    EXPECT_EQ(v.z, 3);
+    EXPECT_EQ(v.w, 4);
+}
+
+TEST(VectorTest, v4Sum) {
+    const Vector4 v1(1, 1, 1, 1), v2(2, 2, 2, 2);
+    const Vector4 v3 = v1 + v2;
+    EXPECT_EQ(v3.x, 3);
+    EXPECT_EQ(v3.y, 3);
+    EXPECT_EQ(v3.z, 3);
+    EXPECT_EQ(v3.w, 3);
+}
+
+TEST(VectorTest, v4Sub) {
+    const Vector4 v1(1, 1, 1, 1), v2(2, 2, 2, 2);
+    const Vector4 v3 = v1 - v2;
+    EXPECT_EQ(v3.x, -1);
+    EXPECT_EQ(v3.y, -1);
+    EXPECT_EQ(v3.z, -1);
+    EXPECT_EQ(v3.w, -1);
+}
+
+TEST(VectorTest, v4Mul) {
+    const Vector4 v1(2, 3, 5, 7), v2(2, 2, 2, 2);
+    const Vector4 v3 = v1 * v2;
+    EXPECT_EQ(v3.x, 4);
+    EXPECT_EQ(v3.y, 6);
+    EXPECT_EQ(v3.z, 10);
+    EXPECT_EQ(v3.w, 14);
+}
+
+TEST(VectorTest, v4Div) {
+    const Vector4 v1(4, 4, 4, 4);
+    const Vector4 v2(2, 2, 2, 2);
+    const Vector4 v3 = v1 / v2;
+    EXPECT_EQ(v3.x, 2);
+    EXPECT_EQ(v3.y, 2);
+    EXPECT_EQ(v3.z, 2);
+    EXPECT_EQ(v3.w, 2);
+}
+
+TEST(VectorTest, v4Neg) {
+    const Vector4 v1(1, 1, 1, 1);
+    const Vector4 v2 = -v1;
+    EXPECT_EQ(v2.x, -1);
+    EXPECT_EQ(v2.y, -1);
+    EXPECT_EQ(v2.z, -1);
+    EXPECT_EQ(v2.w, -1);
+}
+
+TEST(VectorTest, v4MulSca) {
+    const Vector4 v1(1, 1, 1, 1);
+    const Vector4 v2 = v1 * 2;
+    EXPECT_EQ(v2.x, 2);
+    EXPECT_EQ(v2.y, 2);
+    EXPECT_EQ(v2.z, 2);
+    EXPECT_EQ(v2.w, 2);
+}
+
+TEST(VectorTest, v4DivSca) {
+    const Vector4 v1(1, 1, 1, 1);
+    const Vector4 v2 = v1 / 2;
+    EXPECT_EQ(v2.x, 0.5);
+    EXPECT_EQ(v2.y, 0.5);
+    EXPECT_EQ(v2.z, 0.5);
+    EXPECT_EQ(v2.w, 0.5);
+}
+
+TEST(VectorTest, v4Eq) {
+    const Vector4 v1(1, 1, 1, 1);
+    const Vector4 v2(1, 1, 1, 1);
+    const Vector4 v3(1, 2, 2, 2);
+    const Vector4 v4(2, 1, 2, 2);
+    const Vector4 v5(2, 2, 1, 2);
+    const Vector4 v6(2, 2, 2, 1);
+
+    EXPECT_EQ(v1 == v2, true);
+    EXPECT_EQ(v1 == v3, false);
+    EXPECT_EQ(v1 == v4, false);
+    EXPECT_EQ(v1 == v5, false);
+    EXPECT_EQ(v1 == v6, false);
+}
+
+TEST(VectorTest, v4NEq) {
+    const Vector4 v1(1, 1, 1, 1);
+    const Vector4 v2(1, 1, 1, 1);
+    const Vector4 v3(1, 2, 2, 2);
+    const Vector4 v4(2, 1, 2, 2);
+    const Vector4 v5(2, 2, 1, 2);
+    const Vector4 v6(2, 2, 2, 1);
+
+    EXPECT_EQ(v1 != v2, false);
+    EXPECT_EQ(v1 != v3, true);
+    EXPECT_EQ(v1 != v4, true);
+    EXPECT_EQ(v1 != v5, true);
+    EXPECT_EQ(v1 != v6, true);
+}
+
+TEST(VectorTest, v4Len) {
+    const Vector4 v1(4, 4, 4, 4);
+
+    EXPECT_EQ(v1.length(), 8);
+}
+
+TEST(VectorTest, v4Norm) {
+    const Vector4 v1(0, 4, 0, 0);
+    const Vector4 v2(0, 1, 0, 0);
+    const Vector4 v3(4, 4, 4, 4);
+
+    EXPECT_EQ(v1.normalize(), v2);
+    EXPECT_EQ(v2.normalize(), v2);
+    EXPECT_NEAR(v3.normalize().length(), 1, 0.001);
+}
+
+TEST(VectorTest, v4Dot) {
+    const Vector4 v1(0, 0, 0, 1);
+    const Vector4 v2(0, 0, 1, 0);
+    const Vector4 v3(0, 1, 0, 0);
+    const Vector4 v4(1, 0, 0, 0);
+    const Vector4 v5(2, 3, 5, 7);
+    const Vector4 v6(7, 5, 3, 2);
+
+    EXPECT_EQ(v1.dot(v2), 0);
+    EXPECT_EQ(v1.dot(v3), 0);
+    EXPECT_EQ(v1.dot(v4), 0);
+    EXPECT_EQ(v2.dot(v3), 0);
+    EXPECT_EQ(v2.dot(v4), 0);
+    EXPECT_EQ(v3.dot(v4), 0);
+    EXPECT_EQ(v5.dot(v6), 58);
+}
