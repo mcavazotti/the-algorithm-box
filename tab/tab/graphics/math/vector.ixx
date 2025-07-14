b1b095591b2136bb637d0754cbbc6d2c9a8c8e4c
Matheus M Cavazotti <mcavazotti@gmail.com>
2025-07-14 12:42:26 -0300

    Implement Vector3

--- tab/tab/graphics/math/vector.ixx
+++ tab/tab/graphics/math/vector.ixx
@@ -37,8 +37,35 @@ public:
     // end-length-ops
 
     // start-dot
-    [[nodiscard]] double dot(const Vector2& other) const { return x * other.x + y * other.y; };
+    [[nodiscard]] double dot(const Vector2& other) const { return x * other.x + y * other.y; }
     // end-dot
 
     double x, y;
+};
+
+export class Vector3 {
+public:
+    Vector3(): x(0),y(0),z(0) {}
+    Vector3(const double x, const double y, const double z): x(x),y(y),z(z) {}
+
+    Vector3 operator+(const Vector3& other) const { return { x + other.x, y + other.y, z + other.z }; }
+    Vector3 operator-(const Vector3& other) const { return { x - other.x, y - other.y, z - other.z }; }
+    Vector3 operator*(const Vector3& other) const { return { x * other.x, y * other.y, z * other.z }; }
+    Vector3 operator/(const Vector3& other) const { return { x / other.x, y / other.y, z / other.z }; }
+    Vector3 operator-() const { return { -x, -y, -z }; };
+    Vector3 operator*(const double other) const { return { x * other, y * other, z * other }; }
+    Vector3 operator/(const double other) const { return { x / other, y / other, z / other }; }
+    bool operator==(const Vector3& other) const { return x == other.x && y == other.y && z == other.z; }
+    bool operator!=(const Vector3& other) const { return !(*this == other); }
+
+    [[nodiscard]] double length() const { return sqrt(x * x + y * y + z * z); };
+    [[nodiscard]] Vector3 normalize() const { return *this / length(); };
+    [[nodiscard]] double dot(const Vector3& other) const { return x * other.x + y * other.y + z * other.z; }
+    
+    // start-cross
+    [[nodiscard]] Vector3 cross(const Vector3& other) const { return {y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x }; }
+    // start-cross
+
+
+    double x, y, z;
 };
--- tab/tests/graphics/math/vector_test.cpp
+++ tab/tests/graphics/math/vector_test.cpp
@@ -121,3 +121,140 @@ TEST(VectorTest, v2Dot) {
     EXPECT_EQ(v2.dot(v3), 2);
     EXPECT_EQ(v3.dot(v4), 21);
 }
+
+TEST(VectorTest, v3DefaultConstructor) {
+    const Vector3 v;
+    EXPECT_EQ(v.x, 0);
+    EXPECT_EQ(v.y, 0);
+    EXPECT_EQ(v.z, 0);
+}
+
+TEST(VectorTest, v3Initialization) {
+    const Vector3 v(1, 2, 3);
+    EXPECT_EQ(v.x, 1);
+    EXPECT_EQ(v.y, 2);
+    EXPECT_EQ(v.z, 3);
+}
+
+TEST(VectorTest, v3Sum) {
+    const Vector3 v1(1, 1, 1), v2(2, 2, 2);
+    const Vector3 v3 = v1 + v2;
+    EXPECT_EQ(v3.x, 3);
+    EXPECT_EQ(v3.y, 3);
+    EXPECT_EQ(v3.z, 3);
+}
+
+TEST(VectorTest, v3Sub) {
+    const Vector3 v1(1, 1, 1), v2(2, 2, 2);
+    const Vector3 v3 = v1 - v2;
+    EXPECT_EQ(v3.x, -1);
+    EXPECT_EQ(v3.y, -1);
+    EXPECT_EQ(v3.z, -1);
+}
+
+TEST(VectorTest, v3Mul) {
+    const Vector3 v1(2, 3, 5), v2(2, 2, 2);
+    const Vector3 v3 = v1 * v2;
+    EXPECT_EQ(v3.x, 4);
+    EXPECT_EQ(v3.y, 6);
+    EXPECT_EQ(v3.z, 10);
+}
+
+TEST(VectorTest, v3Div) {
+    const Vector3 v1(4, 4, 4);
+    const Vector3 v2(2, 2, 2);
+    const Vector3 v3 = v1 / v2;
+    EXPECT_EQ(v3.x, 2);
+    EXPECT_EQ(v3.y, 2);
+    EXPECT_EQ(v3.z, 2);
+}
+
+TEST(VectorTest, v3Neg) {
+    const Vector3 v1(1, 1, 1);
+    const Vector3 v2 = -v1;
+    EXPECT_EQ(v2.x, -1);
+    EXPECT_EQ(v2.y, -1);
+    EXPECT_EQ(v2.z, -1);
+}
+
+TEST(VectorTest, v3MulSca) {
+    const Vector3 v1(1, 1, 1);
+    const Vector3 v2 = v1 * 2;
+    EXPECT_EQ(v2.x, 2);
+    EXPECT_EQ(v2.y, 2);
+    EXPECT_EQ(v2.z, 2);
+}
+
+TEST(VectorTest, v3DivSca) {
+    const Vector3 v1(1, 1, 1);
+    const Vector3 v2 = v1 / 2;
+    EXPECT_EQ(v2.x, 0.5);
+    EXPECT_EQ(v2.y, 0.5);
+    EXPECT_EQ(v2.z, 0.5);
+}
+
+TEST(VectorTest, v3Eq) {
+    const Vector3 v1(1, 1, 1);
+    const Vector3 v2(1, 1, 1);
+    const Vector3 v3(1, 2, 2);
+    const Vector3 v4(2, 1, 2);
+    const Vector3 v5(2, 2, 1);
+
+    EXPECT_EQ(v1 == v2, true);
+    EXPECT_EQ(v1 == v3, false);
+    EXPECT_EQ(v1 == v4, false);
+    EXPECT_EQ(v1 == v5, false);
+}
+
+TEST(VectorTest, v3NEq) {
+    const Vector3 v1(1, 1, 1);
+    const Vector3 v2(1, 1, 1);
+    const Vector3 v3(1, 2, 2);
+    const Vector3 v4(2, 1, 2);
+    const Vector3 v5(2, 2, 1);
+
+    EXPECT_EQ(v1 != v2, false);
+    EXPECT_EQ(v1 != v3, true);
+    EXPECT_EQ(v1 != v4, true);
+    EXPECT_EQ(v1 != v5, true);
+}
+
+TEST(VectorTest, v3Len) {
+    const Vector3 v1(2, 3, 6);
+
+    EXPECT_EQ(v1.length(), 7);
+}
+
+TEST(VectorTest, v3Norm) {
+    const Vector3 v1(0, 4, 0);
+    const Vector3 v2(0, 1, 0);
+    const Vector3 v3(2, 3, 6);
+
+    EXPECT_EQ(v1.normalize(), v2);
+    EXPECT_EQ(v2.normalize(), v2);
+    EXPECT_NEAR(v3.normalize().length(), 1, 0.001);
+}
+
+TEST(VectorTest, v3Dot) {
+    const Vector3 v1(0, 0, 1);
+    const Vector3 v2(0, 1, 0);
+    const Vector3 v3(1, 0, 0);
+    const Vector3 v4(2, 3, 5);
+    const Vector3 v5(7, 5, 3);
+
+    EXPECT_EQ(v1.dot(v2), 0);
+    EXPECT_EQ(v1.dot(v3), 0);
+    EXPECT_EQ(v2.dot(v3), 0);
+    EXPECT_EQ(v4.dot(v5), 44);
+}
+
+TEST(VectorTest, v3Cross) {
+    const Vector3 v1(0, 0, 1);
+    const Vector3 v2(0, 1, 0);
+    const Vector3 v3(1, 0, 0);
+    const Vector3 v4(1, 2, 3);
+    const Vector3 v5(0, 3, -2);
+
+    EXPECT_EQ(v1.cross(v2), -v3);
+    EXPECT_EQ(v4.cross(v3), v5);
+}
